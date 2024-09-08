6d6bc7215a74693c40dd08f3c76133f7047232b1
Shuntaro Tsunoda <tsunoda@pezy.co.jp>
2024-09-09 00:36:06 +0900

    Add sim, syn

---
+++ syn/arty.tcl
@@ -0,0 +1,95 @@
+#
+# Parameters
+#
+
+if {![catch {set board_files $env(BOARD_FILES)}]} {
+    set_param board.repoPaths $board_files
+}
+set_param general.maxThreads 8
+
+set top    {arty_top}
+set part   {xc7a35ticsg324-1L}
+set board  {digilentinc.com:arty:part0:1.1}
+set debug  0
+set output {output}
+
+set inc_list    {}
+set ip_dir      {../ip/output}
+set file_list   {}
+set xdc_list    {arty.xdc}
+
+set veryl [open ../arty.f r]
+set veryl_list [split [read $veryl] "\n"]
+close $veryl
+
+foreach f $veryl_list {
+    if {[string length $f] > 0} { # remove whitespace line
+        lappend file_list $f
+    }
+}
+
+#
+# Project
+#
+
+set_part $part
+
+#
+# IP
+#
+
+read_ip [glob -directory $ip_dir [file join * {*.xci}]]
+
+#
+# Read files
+#
+
+# set_property INCLUDE_DIRS $inc_list [current_fileset]
+
+foreach f $file_list { read_verilog -sv $f }
+foreach f $xdc_list  { read_xdc $f }
+
+#
+# Synthesis
+#
+
+synth_design -top $top -part $part -flatten_hierarchy none -directive Default
+
+#
+# Chipscope
+#
+if {$debug} {
+    read_xdc debug.xdc
+    implement_debug_core [get_debug_cores]
+}
+
+#
+# Implementation
+#
+
+opt_design
+place_design
+phys_opt_design
+route_design
+
+#
+# Report
+#
+
+report_datasheet                 -file $output/datasheet.txt
+report_io                        -file $output/io.rpt
+report_clocks                    -file $output/clock.rpt
+report_timing_summary            -file $output/timing.rpt -max_paths 10
+report_utilization               -file $output/util.rpt
+# report_clock_utilization         -file $output/util.rpt -append
+# report_ram_utilization           -file $output/util.rpt -append -detail
+report_high_fanout_nets          -file $output/fanout.rpt -timing -load_types -max_nets 25
+report_drc                       -file $output/drc.rpt
+
+#
+# Output
+#
+
+write_checkpoint   -force $output/$top
+write_bitstream    -force $output/$top
+write_debug_probes -force $output/$top
